9a7ce91d8a9460aaac47b292fea52e9186c94824
canawait <nkwangcan@foxmail.com>
2015-11-22 01:02:36 +0800

    add main file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: nkawait
+	> Mail: nkwangcan@foxmail.com 
+	> Created Time: Sun 22 Nov 2015 01:01:07 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char*argv[]) {
+	return 0;
+}
